8359facbce106a91adb895d22559f2ea34f72f20
Daniel Gruno <humbedooh@apache.org>
2023-02-13 00:37:12 +0100

    POST is always implied, no need to test for it

--- server/app/endpoints/mailinglist.py
+++ server/app/endpoints/mailinglist.py
@@ -49,86 +49,82 @@ def can_manage_domain(session: dict, domain: str):
 @asfuid.session_required
 async def process(form_data, session):
     # Creating a new mailing list
-    if quart.request.method == "POST":
-        listpart = form_data.get("listpart")
-        domainpart = form_data.get("domainpart")
-        moderators = form_data.get("moderators")
-        is_private = form_data.get("private", False)
-        muopts = form_data.get("muopts")
-        trailer = form_data.get("trailer", False)
-        expedited = form_data.get("expedited")
-        now = int(time.time())
-        # Validate data
-        try:
-            assert listpart and VALID_LISTPART_RE.match(
-                listpart
-            ), "Invalid list name. Must only consist of alphanumerical characters and dashes"
-            assert listpart.endswith("-digest") is False, "A mailing list cannot end in -digest"
-            assert (
-                domainpart in config.messaging.mail_mappings.values()
-            ), "Mailing list domain is not a valid ASF hostname"
-            assert can_manage_domain(
-                session, domainpart
-            ), "You are not authorized to create mailing lists for this domain"
-            assert isinstance(moderators, list) and moderators, "You need to provide a list of moderators"
-            assert all(
-                VALID_EMAIL_RE.match(moderator) for moderator in moderators
-            ), "Invalid moderator list provided. Please use valid email addresses only"
-            assert not is_private or (
-                listpart in PRIVATE_LISTS or session.root is True
-            ), "Only private@ or security@ can be made private by default. Please file a ticket with Infrastructure for non-standard private lists"
-            assert is_private or listpart not in PRIVATE_LISTS, "private@ and security@ lists MUST be marked as private"
-            assert muopts in VALID_MUOPTS, "Invalid moderation options given"
-            assert isinstance(trailer, bool), "Trailer option must be a boolean value"
-            assert not expedited or session.root, "Only infrastructure can expedite mailing list requests"
-            assert f"{listpart}@{domainpart}" not in config.messaging.mailing_lists, "This mailing already exists"
-        except AssertionError as e:
-            return {"success": False, "message": str(e)}
 
-        # Generate the payload for mailreq
-        request_time = now
-        if expedited:  # If expedited request, for backwards compat, we pretend it came in a day earlier.
-            request_time -= 86400
-        payload = {
-            "type": "mailinglist",
+    listpart = form_data.get("listpart")
+    domainpart = form_data.get("domainpart")
+    moderators = form_data.get("moderators")
+    is_private = form_data.get("private", False)
+    muopts = form_data.get("muopts")
+    trailer = form_data.get("trailer", False)
+    expedited = form_data.get("expedited")
+    now = int(time.time())
+    # Validate data
+    try:
+        assert listpart and VALID_LISTPART_RE.match(
+            listpart
+        ), "Invalid list name. Must only consist of alphanumerical characters and dashes"
+        assert listpart.endswith("-digest") is False, "A mailing list cannot end in -digest"
+        assert domainpart in config.messaging.mail_mappings.values(), "Mailing list domain is not a valid ASF hostname"
+        assert can_manage_domain(session, domainpart), "You are not authorized to create mailing lists for this domain"
+        assert isinstance(moderators, list) and moderators, "You need to provide a list of moderators"
+        assert all(
+            VALID_EMAIL_RE.match(moderator) for moderator in moderators
+        ), "Invalid moderator list provided. Please use valid email addresses only"
+        assert not is_private or (
+            listpart in PRIVATE_LISTS or session.root is True
+        ), "Only private@ or security@ can be made private by default. Please file a ticket with Infrastructure for non-standard private lists"
+        assert is_private or listpart not in PRIVATE_LISTS, "private@ and security@ lists MUST be marked as private"
+        assert muopts in VALID_MUOPTS, "Invalid moderation options given"
+        assert isinstance(trailer, bool), "Trailer option must be a boolean value"
+        assert not expedited or session.root, "Only infrastructure can expedite mailing list requests"
+        assert f"{listpart}@{domainpart}" not in config.messaging.mailing_lists, "This mailing already exists"
+    except AssertionError as e:
+        return {"success": False, "message": str(e)}
+
+    # Generate the payload for mailreq
+    request_time = now
+    if expedited:  # If expedited request, for backwards compat, we pretend it came in a day earlier.
+        request_time -= 86400
+    payload = {
+        "type": "mailinglist",
+        "requester": session.uid,
+        "requested": request_time,
+        "domain": domainpart,
+        "list": listpart,
+        "muopts": muopts,
+        "private": is_private,
+        "mods": moderators,
+        "trailer": "t" if trailer else "T",
+        "expedited": expedited,
+    }
+
+    # Save payload file
+    filename = f"mailinglist-{listpart}-{domainpart}.json"
+    filepath = os.path.join(config.storage.queue_dir, filename)
+    with open(filepath, "w") as f:
+        json.dump(payload, f)
+
+    # Notify of pending request
+    visitype = "private" if is_private else "public"
+    await log.slack(
+        f"A new {visitype} mailing list, `{listpart}@{domainpart}` has been queued for creation, as requested by {session.uid}@apache.org."
+    )
+
+    email.from_template(
+        "mailinglist_create.txt",
+        recipient=("private@infra.apache.org", f"{session.uid}@apache.org"),
+        variables={
+            "listpart": listpart,
+            "domainpart": domainpart,
             "requester": session.uid,
-            "requested": request_time,
-            "domain": domainpart,
-            "list": listpart,
-            "muopts": muopts,
-            "private": is_private,
-            "mods": moderators,
-            "trailer": "t" if trailer else "T",
-            "expedited": expedited,
-        }
+        },
+    )
 
-        # Save payload file
-        filename = f"mailinglist-{listpart}-{domainpart}.json"
-        filepath = os.path.join(config.storage.queue_dir, filename)
-        with open(filepath, "w") as f:
-            json.dump(payload, f)
-
-        # Notify of pending request
-        visitype = "private" if is_private else "public"
-        await log.slack(
-            f"A new {visitype} mailing list, `{listpart}@{domainpart}` has been queued for creation, as requested by {session.uid}@apache.org."
-        )
-
-        email.from_template(
-            "mailinglist_create.txt",
-            recipient=("private@infra.apache.org", f"{session.uid}@apache.org"),
-            variables={
-                "listpart": listpart,
-                "domainpart": domainpart,
-                "requester": session.uid,
-            },
-        )
-
-        # All done for now
-        return {
-            "success": True,
-            "message": "Request logged. Please allow for up to 24 hours for the request to be processed.",
-        }
+    # All done for now
+    return {
+        "success": True,
+        "message": "Request logged. Please allow for up to 24 hours for the request to be processed.",
+    }
 
 
 quart.current_app.add_url_rule(
