34a0cd3f1ac9c07d5fbdccb4d3c26c614bcdc867
Daniel Gruno <humbedooh@apache.org>
2023-05-24 11:04:51 -0500

    don't allow any guff

--- server/app/endpoints/jira_activate_account.py
+++ server/app/endpoints/jira_activate_account.py
@@ -105,6 +105,8 @@ async def process_reactivation_request(formdata):
     """
     jira_username = formdata.get("username")
     jira_email = formdata.get("email")
+    if jira_email.lower().endswith("@apache.org"):  # This is LDAP operated, don't touch!
+        return {"success": False, "message": "Reactivation of internal ASF accounts cannot be done through this tool."}
     if jira_username and jira_username in JIRA_EMAIL_MAPPINGS:
         if JIRA_EMAIL_MAPPINGS[jira_username].lower() == jira_email.lower():  # We have a match!
             # Generate and send confirmation link
