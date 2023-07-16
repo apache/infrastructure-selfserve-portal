f8ae82a8ac6525de0581fe870ada0ade7280904c
Daniel Gruno <humbedooh@apache.org>
2023-07-16 12:43:30 +0200

    NR: If 502, just go straight to toast()

--- htdocs/js/selfserve.js
+++ htdocs/js/selfserve.js
@@ -73,6 +73,9 @@ async function GET(url, params, method = 'GET') {
     body: data,
   });
   log(`[${xhrID}] Server responded to ${method} ${url} with status: ${response.status} ${response.statusText}`);
+  if (response.status === 502) { // Proxy error??
+      toast(response.statusText);
+  }
   return response;
 }
 
